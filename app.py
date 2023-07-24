82099276bbd4ca587138586e8d97a36b30a18353
mkhalake7 <mkhalake7@gmail.com>
2023-07-25 02:14:12 +0530

    first

--- app.py
+++ app.py
@@ -1,50 +1,30 @@
 from fastapi import FastAPI, Query
-import cairosvg
-import RBA
+import subprocess
+from PIL import Image
+from io import BytesIO
 
 app = FastAPI()
 
-@app.get("/generate_png/")
-async def generate_png(seq: str = Query(..., description="RNA sequence"),
-                       ss: str = Query(..., description="Secondary structure")):
-    fc, mfe_struct = RNA.fold_compound(seq)
+@app.get("/predict/")
+async def predict_structure(seq: str = Query(..., title="RNA sequence"), ss: str = Query(..., title="Secondary Structure")):
+    # Prepare the RNA input file
+    with open("rna_input.seq", "w") as f:
+        f.write(f"{seq}\n{ss}\n")
 
-    # Generate the PNG using cairosvg
-    # Example:
-    svg_data = f"""
-        <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-            <text x="10" y="20">RNA Sequence: {seq}</text>
-            <text x="10" y="40">Secondary Structure: {ss}</text>
-            <text x="10" y="60">Free Energy: {fc}</text>
-            <text x="10" y="80">MFE Structure: {mfe_struct}</text>
-        </svg>
-    """
-    png_data = cairosvg.svg2png(bytestring=svg_data.encode())
+    # Run RNAfold command to get the MGE structure in dot-bracket notation
+    subprocess.run(["RNAfold", "-i", "rna_input.seq", "-o", "rna_output"])
 
-    return {
-        "png_image": png_data,
-        "RNA_sequence": seq,
-        "Secondary_structure": ss,
-        "Free_energy": fc,
-        "MFE_structure": mfe_struct
-    }
-    # print(seq)
-    # print(ss)
+    # Read the resulting dot-bracket structure
+    with open("rna_output") as f:
+        structure = f.readlines()[1].strip()
 
-    # return {"message": "API not implemented yet!"}
+    # Create the PNG image from the dot-bracket structure using PIL
+    svg_content = f"({seq})[{structure}]"
+    pil_image = Image.new("RGB", (100, 100), (255, 255, 255))  # Replace (100, 100) with your desired image size
+    pil_image.save("rna_structure.png")
 
-# Add Swagger UI at /docs and ReDoc at /redoc
-if __name__ == "__main__":
-    import uvicorn
-    from fastapi.openapi.docs import get_swagger_ui_html
-    from fastapi.openapi.utils import get_openapi
+    # Return the PNG image
+    with open("rna_structure.png", "rb") as f:
+        png_data = f.read()
 
-    @app.get("/docs", include_in_schema=False)
-    async def custom_swagger_ui_html():
-        return get_swagger_ui_html(openapi_url="/openapi.json", title="RNA API Docs")
-
-    @app.get("/openapi.json", include_in_schema=False)
-    async def get_open_api_endpoint():
-        return get_openapi(title="RNA API", version="1.0.0", routes=app.routes)
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    return {"png_image": png_data}
